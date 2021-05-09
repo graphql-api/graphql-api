03863ed5fd40bdb4cdc1850d30716895666d4ffa
jfa <git@jonathanallenberg.de>
2021-05-09 19:33:34 +0200

    …

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   projects: [
     '<rootDir>/apps/example',
     '<rootDir>/packages/api/stripe',
-    '<rootDir>/packages/api/youtube'
+    '<rootDir>/packages/api/youtube',
+    '<rootDir>/packages/tools'
   ]
 }
